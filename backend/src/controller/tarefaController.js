bb128366a30ad616b9f27fa484074677a80e1087
Cauê Vitor <vitorcaue368@gmail.com>
2024-10-18 14:27:01 -0300

    updateTarefa, getTarefaStatus

--- backend/src/controller/tarefaController.js
+++ backend/src/controller/tarefaController.js
@@ -1,6 +1,7 @@
 import Tarefa from "../models/tarefaModel.js"
 import { z } from "zod"
 
+//validações
 const createSchema = z.object({
     tarefa: z.string({
         invalid_type_error: "A tarefa deve ser um texto",
@@ -13,11 +14,18 @@ const createSchema = z.object({
             .min(5, { messge: "A descrição deve conter no mínimo 5 caracteres" })
             .nullable(),
 })
-
 const idSchema = z.object({
     id: z.string().uuid({ message: 'ID inválido.' })
 })
+const updateSchema = z.object({
+    tarefa: z.string().min(3, { message: "A tarefa deve conter no mínimo 3 caracteres." }).max(255, { message: "A tarefa deve conter no máximo 255 caracteres." }),
+    status: z.enum(["pendente", "concluida"])
+})
+const situacaoSchema = z.object({
+    situacao: z.enum(["pendente", "concluida"])
+})
 
+//controladores
 export const create = async (request, response) => {
     const createValidation = createSchema.safeParse(request.body)
     if (!createValidation.success) {
@@ -42,9 +50,8 @@ export const create = async (request, response) => {
     }
 
 }
-
-//GET -> 3333/api/tarefas?pages=1&limit=10
 export const getAll = async (request, response) => {
+    //GET -> 3333/api/tarefas?pages=1&limit=10
 
     const page = parseInt(request.query.page) || 1
     const limit = parseInt(request.query.limit) || 10
@@ -93,8 +100,40 @@ export const getTarefa = async (request, response) => {
     }
 
 }
-export const updateTarefa = (request, response) => {
-    response.status(200).json("updateTarefa")
+export const updateTarefa = async (request, response) => {
+    const idValidation = idSchema.safeParse(request.params)
+    if (!idValidation.success) {
+        response.status(400).json({ message: idValidation.error })
+        return
+    }
+
+    const id = idValidation.data.id
+
+    const updateValidation = updateSchema.safeParse(request.body)
+    if (!updateValidation.success) {
+        response.status(400).json({ message: updateValidation.error })
+        return
+    }
+
+    const { tarefa, status } = updateValidation.data
+
+    const descricao = request.body.descricao || ""
+
+    const tarefaAtualizada = { tarefa, descricao, status }
+
+    try {
+        const [numberAffectRow] = await Tarefa.update(tarefaAtualizada, { where: { id } })
+
+        if (numberAffectRow <= 0) {
+            response.status(404).json({ err: "Tarefa não encontrada." })
+            return
+        }
+        response.status(500).json({ message: "Tarefa atualizada com sucesso." })
+    } catch (error) {
+        console.error(error)
+        response.status(500).json({ err: "Erro ao atualizar a tarefa." })
+    }
+
 }
 export const updateStatusTarefa = async (request, response) => {
     const idValidation = idSchema.safeParse(request.params)
@@ -121,13 +160,25 @@ export const updateStatusTarefa = async (request, response) => {
         response.status(200).json(tarefaAtulizada)
     } catch (error) {
         console.error(error)
-        response.status(500).json({err: "Erro ao buscar tarefa"})
+        response.status(500).json({ err: "Erro ao buscar tarefa" })
     }
 }
-export const getTarefaStatus = (request, response) => {
-    response.status(200).json("getTarefaStatus")
-}
+export const getTarefaStatus = async (request, response) => {
+    const situacaoValidation = situacaoSchema.safeParse(request.params)
+    if (!situacaoValidation.success) {
+        response.status(400).json({ message: situacaoValidation.error })
+        return
+    }
+    const { situacao } = situacaoValidation.data
 
+    try {
+        const tarefas = await Tarefa.findAll({ where: { status: situacao } })
+        response.status(200).json({ tarefas })
+    } catch (error) {
+        console.error(error)
+        response.status(500).json({ message: "Erro ao buscar tarefas por situação." })
+    }
+}
 export const deleteTarefa = async (request, response) => {
     const idValidation = idSchema.safeParse(request.params)
     if (!idValidation.success) {
